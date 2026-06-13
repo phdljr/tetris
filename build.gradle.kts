2e110aa7440248cb09f3fbbb7b973ecc16cd53c8
phdljr <phdljr@naver.com>
2026-06-13 21:22:55 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    java
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+group = "me.phdljr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.12.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainClass.set("me.phdljr.tetris.HelloApplication")
+}
+
+javafx {
+    version = "26.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jlink {
+    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "app"
+    }
+}
