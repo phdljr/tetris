39b33f7674a59c6568faed3817a8d9e7a5c72fe7
phdljr <phdljr@naver.com>
2026-08-17 20:10:31 +0900

    init

--- src/main/java/me/phdljr/tetris/Launcher.java
+++ src/main/java/me/phdljr/tetris/Launcher.java
@@ -3,7 +3,7 @@ package me.phdljr.tetris;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
 }
